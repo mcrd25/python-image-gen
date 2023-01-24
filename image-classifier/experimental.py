ecc41254801413b4f23d35c0953b45e3fadeaf5f
mcrd25 <mayacdouglas@gmail.com>
2023-01-24 02:31:25 -0400

    small change for testing purposes

--- image-classifier/experimental.py
+++ image-classifier/experimental.py
@@ -10,5 +10,7 @@ dataset_url = "https://1drv.ms/u/s!AkGrCMoO9SmmjftyREzLbHuIINZNYA?e=jvapfa"
 data_dir = tf.keras.utils.get_file('ai-images-dataset', origin=dataset_url, untar=True)
 data_dir = pathlib.Path(data_dir)
 
+print(data_dir)
+
 image_count = len(list(data_dir.glob('*/*.jpg')))
 print(image_count)
